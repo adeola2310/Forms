a3af08b478eef5d64e8c5410caabf59c2b3cf0fa
adeola2310 <adekoyejoadeola@gmail.com>
2020-02-10 18:48:45 +0100

    refactored design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from "jquery";
 import './App.css';
 import {isOnlyText,
   isEmailValid,
@@ -7,6 +8,7 @@ import {isOnlyText,
   isPassword,
   confirmPassword,
   isNumber,
+
     formatCardNumber
   } from "./validators/helpers";
 
@@ -101,7 +103,7 @@ class App extends React.Component{
          this.setState(prevState => ({
            error: {
              ...prevState.error,
-             number: 'number is invalid'
+             number: 'number is invalid and must start with Nigerian number eg 080,070,090'
            }
          }))
         }
@@ -115,22 +117,8 @@ class App extends React.Component{
         }
         break;
       case 'cardNumber':
-        if ( value && !formatCardNumber(value)){
-         this.setState(prevState => ({
-           error: {
-             ...prevState.error,
-             cardNumber: 'card number is invalid'
-           }
-         }))
-        }
-        else{
-          this.setState(prevState => ({
-            error: {
-              ...prevState.error,
-              cardNumber: ''
-            }
-          }))
-        }
+
+
         break;
       case 'date':
         let date = document.getElementById('date');
@@ -170,7 +158,7 @@ class App extends React.Component{
          this.setState(prevState => ({
            error: {
              ...prevState.error,
-             password: 'password is invalid'
+             password: 'password must contain At least 1 uppercase character,1 number, 1 special character and not shorter than 6 characters'
            }
          }))
         }
@@ -238,117 +226,210 @@ class App extends React.Component{
   render() {
     const { user, error } = this.state;
     return (
-        <div className="wrapper">
-          <div className="form-wrapper">
-            <div className="details">
-              <h2 className="head"> Fill in the Details</h2>
-              <form className="row" onSubmit={this.handleSubmit}  noValidate>
-                <div className="column">
-                  <div className="input-group">
-                    <label htmlFor="FullName">Full Name:</label>
-                    <input
-                        type="text"
-                        placeholder="John Doe"
-                        name="fullName"
-                        value={user.fullName}
-                        onChange={this.handleChange}
-                        className={ `${error.fullName}`}
-                    />
-                  </div>
-                  <span className="error-message">{error.fullName}</span>
+        // <div className="wrapper">
+        //   <div className="form-wrapper">
+        //     <div className="details">
+        //       <h2 className="head"> Fill in the Details</h2>
+        //       <form className="row" onSubmit={this.handleSubmit}  noValidate>
+        //         <div className="column">
+        //           <div className="input-group">
+        //             <label htmlFor="FullName">Full Name:</label>
+        //             <input
+        //                 type="text" required=""
+        //                 placeholder="John Doe"
+        //                 name="fullName"
+        //                 value={user.fullName}
+        //                 onChange={this.handleChange}
+        //                 className={ `${error.fullName}`}
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.fullName}</span>
+        //
+        //           <div className="input-group">
+        //             <label htmlFor="email">Email:</label>
+        //             <input
+        //                 type="email"
+        //                 placeholder="JohnDoe@gmail.com"
+        //                 name="email"
+        //                 className={ `${error.email}`}
+        //                 onChange={this.handleChange}
+        //             />
+        //           </div>
+        //
+        //           <span className="error-message">{error.email}</span>
+        //
+        //           <div className="input-group">
+        //             <label htmlFor="number">Phone Number:</label>
+        //             <input
+        //                 name="number"
+        //                 type="number"
+        //                 maxLength="10"
+        //                 value={user.number}
+        //                 onChange={this.handleChange}
+        //                 className={`${error.number}`}
+        //             />
+        //           </div>
+        //           <span className="error-message"> {error.number}</span>
+        //           <div className="input-group">
+        //             <label htmlFor="Password">Password:</label>
+        //             <input
+        //                 type="password"
+        //                 name="password"
+        //                 value={user.password}
+        //                 onChange={this.handleChange}
+        //                 className={`${error.password}`}
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.password}</span>
+        //           <div className="input-group">
+        //             <label htmlFor="confirmPassword">Confirm Password:</label>
+        //             <input
+        //                 type="password"
+        //                 name="confirmPassword"
+        //                 value={user.confirmPassword}
+        //                 onChange={this.handleChange}
+        //                 className={`${error.confirmPassword}`}
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.confirmPassword}</span>
+        //           <div className="input-group">
+        //             <label htmlFor="cardNumber">Card Number:</label>
+        //             <input
+        //                 type="number"
+        //                 value={user.cardNumber}
+        //                 onChange={this.handleChange}
+        //                 className={`${error.cardNumber}`}
+        //                 name="cardNumber"
+        //                 placeholder="XXXX XXXX XXXX XXXX"
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.cardNumber}</span>
+        //           <div className="input-group">
+        //             <label htmlFor="date">Date:</label>
+        //             <input
+        //                 name="date"
+        //                 id="date"
+        //                 type="text"
+        //                 value={user.date}
+        //                   onChange={this.handleChange}
+        //                 className={`${error.date}`}
+        //                 placeholder="MM/YY"
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.password}</span>
+        //           <div className="input-group">
+        //             <label htmlFor="pin">PIN:</label>
+        //             <input
+        //                 type="password"
+        //                 name="pin"
+        //                 maxLength="4"
+        //                 value={user.pin}
+        //                 className={ `${error.pin}`}
+        //                 onChange={this.handleChange}
+        //             />
+        //           </div>
+        //           <span className="error-message">{error.pin}</span>
+        //           <div className="row">
+        //             <button id="submit" className="btn" disabled={!this.isValid}>Submit </button>
+        //           </div>
+        //         </div>
+        /*      </form>*/
+        /*    </div>*/
+        /*  </div>*/
+        /*</div>*/
 
-                  <div className="input-group">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        placeholder="JohnDoe@gmail.com"
-                        name="email"
-                        className={ `${error.email}`}
-                        onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <span className="error-message">{error.email}</span>
-
-                  <div className="input-group">
-                    <label htmlFor="number">Phone Number:</label>
-                    <input
-                        name="number"
-                        type="number"
-                        maxLength="10"
-                        value={user.number}
-                        onChange={this.handleChange}
-                        className={`${error.number}`}
-                    />
-                  </div>
-                  <span className="error-message"> {error.number}</span>
-                  <div className="input-group">
-                    <label htmlFor="Password">Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={user.password}
-                        onChange={this.handleChange}
-                        className={`${error.password}`}
-                    />
-                  </div>
-                  <span className="error-message">{error.password}</span>
-                  <div className="input-group">
-                    <label htmlFor="confirmPassword">Confirm Password:</label>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        value={user.confirmPassword}
-                        onChange={this.handleChange}
-                        className={`${error.confirmPassword}`}
-                    />
-                  </div>
-                  <span className="error-message">{error.confirmPassword}</span>
-                  <div className="input-group">
-                    <label htmlFor="cardNumber">Card Number:</label>
-                    <input
-                        type="number"
-                        value={user.cardNumber}
-                        onChange={this.handleChange}
-                        className={`${error.cardNumber}`}
-                        name="cardNumber"
-                        placeholder="XXXX XXXX XXXX XXXX"
-                    />
-                  </div>
-                  <span className="error-message">{error.cardNumber}</span>
-                  <div className="input-group">
-                    <label htmlFor="date">Date:</label>
-                    <input
-                        name="date"
-                        id="date"
-                        type="text"
-                        value={user.date}
-                          onChange={this.handleChange}
-                        className={`${error.date}`}
-                        placeholder="MM/YY"
-                    />
-                  </div>
-                  <span className="error-message">{error.password}</span>
-                  <div className="input-group">
-                    <label htmlFor="pin">PIN:</label>
-                    <input
-                        type="password"
-                        name="pin"
-                        maxLength="4"
-                        value={user.pin}
-                        className={ `${error.pin}`}
-                        onChange={this.handleChange}
-                    />
-                  </div>
-                  <span className="error-message">{error.pin}</span>
-                  <div className="row">
-                    <button id="submit" className="btn" disabled={!this.isValid}>Submit </button>
-                  </div>
+        <div className="container">
+          <div className="all-container">
+            <div id="Column">
+              <h2>Fill the Card Details</h2>
+              <form id="signupSection" onSubmit={this.handleSubmit}>
+                <div id="inputDivFirstName" className="inputDiv">
+                  <input id="inputFieldFirstName"
+                         className={` ${error.fullName}`}
+                         type="text"
+                         name="fullName"
+                         placeholder="Full Name"
+                         onChange={this.handleChange}
+                  />
                 </div>
+                <span className="error-message">{error.fullName}</span>
+                <div id="inputDivEmail" className="inputDiv">
+                            <input
+                                  type="email"
+                                  placeholder="JohnDoe@gmail.com"
+                                  name="email"
+                                  className={ `${error.email}`}
+                                  onChange={this.handleChange}
+                              />
+                </div>
+                <span className="error-message">{error.email}</span>
+                <div id="inputDivPhone" className="inputDiv" data-fieldname="Password">
+                  <input id="inputDivPhone"
+                         className={ `${error.number}`}
+                         type="number"
+                         placeholder="+234"
+                         name="number"
+                         onChange={this.handleChange}
+                  />
+                </div>
+                <span className="error-message">{error.number}</span>
+                <div id="inputDivPassword" className="inputDiv">
+                  <input id="inputFieldPassword"
+                         className={`${error.password}`}
+                         type="password"
+                         placeholder="Password"
+                         name="password"
+                         onChange={this.handleChange}
+                  />
+                </div>
+                <span className="error-message">{error.password}</span>
+                <div id="inputDivPassword" className="inputDiv">
+                  <input id="inputFieldPassword"
+                         className={`${error.confirmPassword}`}
+                         type="password"
+                         name="confirmPassword"
+                         onChange={this.handleChange}
+                         placeholder="Confirm Password"/>
+                </div>
+                <span className="error-message">{error.confirmPassword}</span>
+                <div id="inputDivDate" className="inputDiv">
+                  <input id="date"
+                         className={`${error.date}`}
+                         type="text"
+                         name="date"
+                         onChange={this.handleChange}
+                         placeholder="MM/YY"/>
+                </div>
+                <div id="inputDivDate" className="inputDiv">
+                  <input id="date"
+                         className={`${error.cardNumber}`}
+                         type="text"
+                         name="cardNumber"
+                         onChange={this.handleChange}
+                         placeholder="XXXX XXXX XXXX XXXX"/>
+                </div>
+                <span className="error-message">{error.cardNumber}</span>
+                <div id="inputDivDate" className="inputDiv">
+                  <input
+                         className={`${error.pin}`}
+                         maxLength="4"
+                         type="password"
+                         name="pin"
+                         onChange={this.handleChange}
+                         placeholder="PIN"
+                        />
+                </div>
+                <span className="error-message">{error.pin}</span>
+                <button id="CTAButton">
+                  SUBMIT
+                </button>
+
               </form>
             </div>
           </div>
+
         </div>
+
     );
   }
 }
